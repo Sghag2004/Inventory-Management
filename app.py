deff5533dc890e33d7e080686d884b3aa5bd7845
Sujal Sadashiv Ghag <sujal.ghag23@aiml.sce.edu.in>
2025-04-07 07:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,5 +95,5 @@ def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
